0c8e2290b134888e1be93b58161fa9ca14adc036
Sébastien Pradier <sebastienpradier@mbpdesebastien.home>
2022-10-09 21:06:16 +0200

    add new

--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import pandas as pd
 import matplotlib.pyplot as mp
+from pyodide.http import open_url
 
-df = pd.read_csv("files/losses.csv")
+df = pd.read_csv(open_url('https://github.com/spradier/spradier.github.io/blob/master/files/losses.csv'))
                         
 df.plot.line(y=["g_losses", "d_losses"])
                         
